09f773662f30b482216c36f26c718665ddc73746
william thornton <WZThornton@hotmail.com>
2022-04-01 14:35:07 -0400

    Add project files.

---
+++ Cours5EX/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace Cours5EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Voiture voiture = new("Toyota");
+            voiture.Accelere(20);
+            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            voiture.Ralentie(10);
+            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            voiture.Arret();
+            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            voiture.Accelere(40);
+            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            voiture.Turbo();
+            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            voiture.POLICEZONESCOLAIRE();
+            WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            voiture.Accelere(150);
+            WriteLine("SPEED : {0}",voiture.RetournerVitesse());
+            voiture.POLICEAUTOROUTE();
+            WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            WriteLine("BRAND : {0}",voiture.RetournerBrand());
+        }
+    }
+}
---
+++ Cours5EX/Voiture.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Cours5EX
+{
+    public class Voiture
+    {
+        private uint vitesse;
+        private string brand;
+
+        public Voiture()
+        {
+            this.vitesse = 0;
+        }
+
+        public Voiture(uint vitesse) 
+        {
+            this.vitesse = vitesse;
+        }
+
+        public Voiture(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public void Accelere(uint Champignon)
+        {
+            vitesse += Champignon;
+        }
+
+        public void Ralentie(uint brake)
+        { 
+        vitesse -= brake;
+        }
+
+        public void Arret()
+        { 
+        vitesse = 0;
+        }
+
+        public void Turbo()
+        {
+            vitesse += 1000;
+        }
+
+        public void POLICEAUTOROUTE()
+        {
+            vitesse = 95;
+        }
+
+        public void POLICEZONESCOLAIRE()
+        {
+            vitesse = 29;
+        }
+
+        public uint RetournerVitesse()
+        { 
+            return vitesse;
+        }
+
+        public string RetournerBrand()
+        { 
+            return brand;
+        }
+    }
+}
