e5be2c6be62f2651a5ff2c85fea5aba4d9f20a9a
william thornton <WZThornton@hotmail.com>
2022-04-01 15:15:40 -0400

    Updated

--- Cours5EX/Program.cs
+++ Cours5EX/Program.cs
@@ -9,15 +9,15 @@ namespace Cours5EX
         {
            Voiture voiture = new("Toyota");
             voiture.Accelere(20);
-            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            WriteLine("SPEED : {0}", voiture.RetournerVitesse());
             voiture.Ralentie(10);
-            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            WriteLine("SPEED : {0}", voiture.RetournerVitesse());
             voiture.Arret();
-            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            WriteLine("SPEED : {0}", voiture.RetournerVitesse());
             voiture.Accelere(40);
-            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            WriteLine("SPEED : {0}", voiture.RetournerVitesse());
             voiture.Turbo();
-            Console.WriteLine("SPEED : {0}", voiture.RetournerVitesse());
+            WriteLine("SPEED : {0}", voiture.RetournerVitesse());
             voiture.POLICEZONESCOLAIRE();
             WriteLine("SPEED : {0}", voiture.RetournerVitesse());
             voiture.Accelere(150);
